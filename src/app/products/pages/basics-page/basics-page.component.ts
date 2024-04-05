60593b1bc5ef504a2f4ab2e29f0a24ecf15147b6
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-05 16:23:58 +0200

    Uppercase. LowerCase y TitleCase Pipes

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-basics-page',
-  templateUrl: './basics-page.component.html',
-  styleUrl: './basics-page.component.css'
+	selector: 'app-basics-page',
+	templateUrl: './basics-page.component.html',
+	styleUrl: './basics-page.component.css',
 })
 export class BasicsPageComponent {
-
+	public nameLower: string = 'jorge alejandro';
+	public nameUpper: string = 'JORGE ALEJANDRO';
+	public fullName: string = 'jOrGe AlEjAnDrO pArEdEs PrAtO';
 }
