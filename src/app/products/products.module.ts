7a7e0690e1500c1a271fe70d9e5c7ae8fd4a3f1b
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-07 18:44:33 +0200

    Continuación de la aplicación

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -6,9 +6,10 @@ import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { OrderComponent } from './pages/order/order.component';
 
 @NgModule({
-	declarations: [BasicsPageComponent, NumbersPageComponent, UncommonPageComponent],
+	declarations: [BasicsPageComponent, NumbersPageComponent, UncommonPageComponent, OrderComponent],
 	imports: [CommonModule, ProductsRoutingModule, PrimeNgModule],
 })
 export class ProductsModule {}
