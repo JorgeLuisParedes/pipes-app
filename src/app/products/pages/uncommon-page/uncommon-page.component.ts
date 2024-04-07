3b4e3d59086a4c9d45b56282ef8c3eee4d636bac
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-07 02:19:18 +0200

    I18nSelectPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-uncommon-page',
-  templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+	selector: 'app-uncommon-page',
+	templateUrl: './uncommon-page.component.html',
+	styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
+	// i18n Select
+	public name: string = 'Jorge Alejandro';
+	public gender: 'male' | 'female' = 'male';
+	public invitationMap = {
+		male: 'invitarlo',
+		female: 'invitarla',
+	};
 
+	changeClient(): void {
+		this.name = 'Sophia Victoria';
+		this.gender = 'female';
+	}
 }
